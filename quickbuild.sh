acac9f7e8b07db50685c579bc8b49becd4da5cf7
Lars Scheibling <lars@scheibling.se>
2022-07-22 15:01:40 +0000

    Added new build command

--- quickbuild.sh
+++ quickbuild.sh
@@ -1,6 +1,7 @@
-VER=$2
 KEY=$1
+VER=$2
+
 rm -f ./Frends.Community.HIT.FileOps.*.nupkg && \
 dotnet build --configuration Release /p:Version=$VER && \
 dotnet pack --configuration Release /p:Version=$VER --no-build --include-source --output . && \
-dotnet nuget push Frends.Community.HIT.FileOps.$VER.nupkg --source https://rest.hoglandet.se/nuggets/ --api-key $KEY 
+dotnet nuget push Frends.Community.HIT.FileOps.$VER.nupkg --source https://proget.intern.hoglandet.se/nuget/Frends/ --api-key $KEY
